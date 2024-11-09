0ce2d2bde49e81b542d6a6d5948e942b5f574ed3
huzaifanaeem1 <huzaifaneem1407@gmail.com>
2024-11-10 00:00:52 +0500

    first commit

--- src/app/trainer/page.tsx
+++ src/app/trainer/page.tsx
@@ -1,27 +1,28 @@
-// import React from "react";
+import React from "react";
 // import { SectionTitle, Trainers } from "@/components";
-// import "../../styles/trainers/page.module.css"
+import "../../styles/trainers/page.module.css"
 
-// const Page = () => {
-//   return (
-//     <div>
-//       <section className={styles.pageSection}> 
-//         <div className={styles.titleContainer}> 
-//           <div className="w-full">
-//             <SectionTitle first="Our" second="Trainers" />
-//             <p className={styles.sectionDescription}> 
-//               Our certified trainers are dedicated to guiding you every step of the way.
-//               With personalized fitness plans and expert advice, we ensure you reach your goals
-//               efficiently and safely.
-//             </p>
-//           </div>
-//           <div className={styles.trainerContainer}>
-//             <Trainers />
-//           </div>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// };
+const Page = () => {
+  return (
+    <div>
+        hello
+      {/* <section className={styles.pageSection}> 
+        <div className={styles.titleContainer}> 
+          <div className="w-full">
+            <SectionTitle first="Our" second="Trainers" />
+            <p className={styles.sectionDescription}> 
+              Our certified trainers are dedicated to guiding you every step of the way.
+              With personalized fitness plans and expert advice, we ensure you reach your goals
+              efficiently and safely.
+            </p>
+          </div>
+          <div className={styles.trainerContainer}>
+            <Trainers />
+          </div>
+        </div>
+      </section> */}
+    </div>
+  );
+};
 
-// export default Page;
+export default Page;
