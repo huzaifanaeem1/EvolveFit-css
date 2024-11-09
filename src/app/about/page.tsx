f5461b346f0b3d61bc83c6af16edf79638474e68
huzaifanaeem1 <huzaifaneem1407@gmail.com>
2024-11-09 23:46:12 +0500

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,23 @@
-// import React from "react";
+import React from "react";
 // import { About as AboutComp, SectionTitle } from "@/components";
 // import "../../styles/about/About.module.css"
 
-// const About = () => {
-//   return (
-//     <section className={styles.section}>
-//        <SectionTitle first="About" second="Us" />
-//       <p className={styles.sectionParagraph}>
-//         At EvolveFit, fitness is a lifestyle. Our expert trainers, personalized programs,
-//         and state-of-the-art facilities are here to support you every step of the way.
-//         We’re committed to helping you achieve a stronger, healthier version of yourself
-//         through dedication and community.
-//       </p>
-//       <AboutComp />
-//     </section>
-//   );
-// };
+const About = () => {
+  return (
+    <div>
+        hello world
+    </div>
+    // <section className={styles.section}>
+    //    <SectionTitle first="About" second="Us" />
+    //   <p className={styles.sectionParagraph}>
+    //     At EvolveFit, fitness is a lifestyle. Our expert trainers, personalized programs,
+    //     and state-of-the-art facilities are here to support you every step of the way.
+    //     We’re committed to helping you achieve a stronger, healthier version of yourself
+    //     through dedication and community.
+    //   </p>
+    //   <AboutComp />
+    // </section>
+  );
+};
 
-// export default About;
+export default About;
